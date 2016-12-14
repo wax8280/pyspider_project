8c61c61eb87bc22ea39ac7692d828e48f3633792
wax8280 <wax8280@163.com>
2016-12-14 19:35:48 +0800

    update

--- RL/RL0055.py
+++ RL/RL0055.py
@@ -9,6 +9,7 @@ import re
 
 BEGIN = 1
 DIVIDE = 5
+TRY_TIME = 10
 
 
 class Handler(BaseHandler):
@@ -32,34 +33,111 @@ class Handler(BaseHandler):
         self.crawl(
             self.url.format(BEGIN),
             save={
-                'now': BEGIN
+                'now': BEGIN,
+                'try_time': 0
             },
             callback=self.list_page,
             force_update=True,
             headers=self.default_headers,
         )
 
+    @catch_status_code_error
     def list_page(self, response):
 
-        if response.save['now'] >= 4163642:
-            return
+        if response.status_code == 200 and u'tCompany_full' in response.text:
 
-        for i in range(response.save['now'] + 1, response.save['now'] + DIVIDE):
+            if response.save['now'] >= 4163642:
+                return
+
+            for i in range(response.save['now'] + 1, response.save['now'] + DIVIDE):
+                self.crawl(
+                    self.url.format(i),
+                    save={
+                        'now': i,
+                        'try_time': 0
+                    },
+                    callback=self.list_page,
+                    headers=self.default_headers,
+                )
+
+            hidTotal = re.search('<input type="hidden" id="hidTotal" name="hidTotal" value="(.*?)">', response.text)
+
+            if hidTotal:
+                hidTotal = int(hidTotal.group(1))
+                print hidTotal
+                now = 1
+                new_headers = deepcopy(self.default_headers)
+                new_headers['Referer'] = self.url.format(response.save['now'])
+
+                self.crawl(
+                    self.url.format(response.save['now']),
+                    save={
+                        'hidTotal': hidTotal,
+                        'now': now,
+                        'Referer': self.url.format(response.save['now']),
+                        'try_time': 0,
+                    },
+                    method='POST',
+                    data={'hidTotal': hidTotal, 'pageno': now},
+                    callback=self.get_list,
+                    headers=new_headers,
+                )
+
+            return {
+                'content': response.text,
+                'url': response.url,
+            }
+
+        elif response.save['try_time'] < TRY_TIME:
             self.crawl(
-                self.url.format(i),
+                self.url.format(response.save['now']),
                 save={
-                    'now': i,
+                    'now': response.save['now'],
+                    'try_time': response.save['try_time'] + 1
                 },
                 callback=self.list_page,
                 headers=self.default_headers,
             )
 
-        hidTotal = re.search('<input type="hidden" id="hidTotal" name="hidTotal" value="(.*?)">', response.text)
+    @config(priority=2)
+    @catch_status_code_error
+    def get_list(self, response):
 
-        if hidTotal:
-            hidTotal = int(hidTotal.group(1))
-            print hidTotal
-            now = 1
+        if response.status_code == 200 and u'd' in response.text:
+            now = response.save['now']
+            hidTotal = response.save['hidTotal']
+            Referer = response.save['Referer']
+            new_headers = deepcopy(self.default_headers)
+            new_headers['Referer'] = Referer
+
+            for i in response.doc('.t1 a').items():
+                self.crawl(
+                    i.attr.href,
+                    callback=self.get_content,
+                    headers=new_headers,
+                )
+
+            if now * 20 < int(hidTotal):
+                now += 1
+
+                self.crawl(
+                    Referer,
+                    save={
+                        'hidTotal': hidTotal,
+                        'now': now,
+                        'Referer': Referer,
+                        'try_time': 0,
+                    },
+                    method='POST',
+                    data={'hidTotal': hidTotal, 'pageno': now},
+                    callback=self.get_list,
+                    headers=new_headers,
+                )
+
+        elif response.save['try_time'] < TRY_TIME:
+            hidTotal = response.save['hidTotal']
+            now = response.save['now']
+
             new_headers = deepcopy(self.default_headers)
             new_headers['Referer'] = self.url.format(response.save['now'])
 
@@ -69,44 +147,7 @@ class Handler(BaseHandler):
                     'hidTotal': hidTotal,
                     'now': now,
                     'Referer': self.url.format(response.save['now']),
-                },
-                method='POST',
-                data={'hidTotal': hidTotal, 'pageno': now},
-                callback=self.get_list,
-                headers=new_headers,
-            )
-
-        return {
-            'content': response.text,
-            'url': response.url,
-        }
-
-    @config(priority=2)
-    def get_list(self, response):
-
-        now = response.save['now']
-        hidTotal = response.save['hidTotal']
-        Referer = response.save['Referer']
-        new_headers = deepcopy(self.default_headers)
-        new_headers['Referer'] = Referer
-
-        for i in response.doc('.t1 a').items():
-            self.crawl(
-                i.attr.href,
-                callback=self.get_content,
-                headers=new_headers,
-            )
-
-        if now * 20 < int(hidTotal):
-            now += 1
-
-
-            self.crawl(
-                Referer,
-                save={
-                    'hidTotal': hidTotal,
-                    'now': now,
-                    'Referer': Referer,
+                    'try_time': response.save['try_time'] + 1,
                 },
                 method='POST',
                 data={'hidTotal': hidTotal, 'pageno': now},
@@ -115,8 +156,33 @@ class Handler(BaseHandler):
             )
 
     @config(priority=3)
+    @catch_status_code_error
     def get_content(self, response):
-        return {
-            'content': response.text,
-            'url': response.url
-        }
+        if response.status_code == 200 and u'd' in response.text:
+
+            return {
+                'content': response.text,
+                'url': response.url
+            }
+
+        elif response.save['try_time'] < TRY_TIME:
+            Referer = response.save['Referer']
+            now = response.save['now']
+            hidTotal = response.save['hidTotal']
+
+            new_headers = deepcopy(self.default_headers)
+            new_headers['Referer'] = Referer
+
+            self.crawl(
+                Referer,
+                save={
+                    'hidTotal': hidTotal,
+                    'now': now,
+                    'Referer': Referer,
+                    'try_time': response.save['try_time'] + 1,
+                },
+                method='POST',
+                data={'hidTotal': hidTotal, 'pageno': now},
+                callback=self.get_list,
+                headers=new_headers,
+            )
