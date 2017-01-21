a27b9ece69531f03e64c576e2612e2d491b2d462
wax8280 <wax8280@163.com>
2017-01-21 17:26:06 +0800

    update

--- RL/RL0094.py
+++ RL/RL0094.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-# Created on 2017-01-13 14:46:22
-# Project: RL0094
+# Created on 2017-01-21 11:45:55
+# Project: RL0094_2
 
 from pyspider.libs.base_handler import *
 import re
 import time
 import json
 from copy import deepcopy
+from urllib import urlencode
 
 BEGIN = 6350000
 END = 7620000
@@ -32,17 +33,15 @@ class Handler(BaseHandler):
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.8',
-        'Referer': 'http://www.laipigo.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
     }
 
     get_content_headers = {
         'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.8',
         'X-Requested-With': 'XMLHttpRequest',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
-
     }
 
     crawl_config = {
@@ -50,7 +49,7 @@ class Handler(BaseHandler):
         'retries': 5,
     }
 
-    api_url = 'http://10.26.225.178:10265/api/common/RL0094?start={}&end={}'
+    api_url = 'http://120.77.12.100:10265/api/common/RL0094?start={}&end={}'
 
     def on_start(self):
         self.crawl(
@@ -78,59 +77,110 @@ class Handler(BaseHandler):
                        )
 
         for name in names:
+            param = urlencode({'q': name.encode('utf8')})
+            # 异步，真实的API
+            headers = deepcopy(self.get_content_headers)
+            headers.update({'Referer': 'http://www.telecredit.cn/result?' + param})
+
+            param2 = urlencode({
+                'searchKey': name.encode('utf8'),
+                'page': '1',
+                'registerCapitals': '0',
+                'formed': '0',
+                'provice': '%E5%85%A8%E9%83%A8'
+            })
+
             self.crawl(
-                cdtp(name),
+                'http://www.telecredit.cn/search/searchData?' + param2,
                 callback=self.get_search_list,
-                headers=self.default_headers,
-                proxy='localhost:3128',
-                save={'name': name, 'url': cdtp(name)}
+                headers=headers,
+                # proxy='localhost:3128',
+                # 保存url，以便设置Referer
+                save={'name': name,
+                      'search_url': 'http://www.telecredit.cn/result?' + param,
+                      'last_headers': headers}
             )
 
     @config(priority=3)
     def get_search_list(self, response):
-        if response.save['name'] not in response.text:
+        try:
+            result = json.loads(response.text)
+        except:
             self.crawl(
-                cdtp(response.save['name']),
-                callback=self.get_content,
-                headers=self.default_headers,
-                proxy='localhost:3128',
-                save={'name': response.save['name'], 'url': cdtp(response.save['name'])}
+                response.url,
+                callback=self.get_search_list,
+                headers=response.save['last_headers'],
+                # proxy='localhost:3128',
+                save=response.save
             )
             return
 
-        if u'请确认输入的企业名称是否正确' in response.text:
+        for item in result['list']:
+            gen = re.search(u'(.*?)\.信用\.信息', item['uname'])
+
+            if gen:
+                name = gen.group(1)
+
+                headers = deepcopy(self.default_headers)
+                headers.update({
+                    'Referer': response.save['search_url']
+                })
+
+                self.crawl(
+                    cdtp(name),
+                    callback=self.get_company_page,
+                    headers=headers,
+                    # proxy='localhost:3128',
+                    save={'name': name,
+                          'last_headers': headers}
+                )
+
+    @config(priority=5)
+    def get_company_page(self, response):
+        if response.save['name'] not in response.text:
+            self.crawl(
+                response.url,
+                headers=response.save['last_heasers'],
+                callback=self.get_company_page,
+                save=response.save,
+            )
             return
 
         gen = re.search('id="companyId" value="([\d]+)"', response.text)
         if gen:
             headers = deepcopy(self.get_content_headers)
-            headers.update({'Referer': response.save['url']})
-            response.save.update({'headers': headers})
+            headers.update({'Referer': response.url})
+
+            # 异步加载一部分
             self.crawl(
-                response.save['url'] + 'companyInfo/basicInformation?companyId={}&companyName={}'.
+                response.url + u'companyInfo/basicInformation?companyId={}&companyName={}'.
                 format(gen.group(1), response.save['name']),
                 headers=headers,
                 callback=self.get_content,
-                save=response.save,
-                proxy='localhost:3128',
+                save={
+                    'name': response.save['name'],
+                    'last_headers': headers
+                },
+                # proxy='localhost:3128',
             )
 
+        # 同步加载一部分
         return {
             'content': response.save['name'] + '\n' + response.text,
             'url': response.url
         }
 
-    @config(priority=4)
+    @config(priority=10)
     def get_content(self, response):
         try:
             json.loads(response.text)
         except:
             self.crawl(
                 response.url,
-                headers=response.save['headers'],
+                headers=response.save['last_headers'],
                 callback=self.get_content,
                 save=response.save,
-                proxy='localhost:3128',
+                # proxy='localhost:3128',
             )
             return
 
